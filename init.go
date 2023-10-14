9f45534f3aefc6ba48dd04d9433559cadd88e1fd
chenbiao <chenbiao@reta-inc.com>
2023-10-14 21:16:06 +0800

    openai client

--- init.go
+++ init.go
@@ -2,17 +2,13 @@ package dgpt
 
 import (
 	"github.com/sashabaranov/go-openai"
-	"log"
 	"os"
 )
 
 var client *openai.Client
 
 func init() {
-	apiKey, ok := os.LookupEnv("API_KEY")
-	if !ok {
-		log.Fatal("lookup API_KEY env fail")
-	}
+	apiKey := os.Getenv("OPENAI_API_KEY")
 
 	client = openai.NewClient(apiKey)
 }
