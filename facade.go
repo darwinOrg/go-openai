757781345122791c01fae2e2d9bde21952f12df0
chenbiao <chenbiao@reta-inc.com>
2023-10-15 15:20:12 +0800

    test

--- facade.go
+++ facade.go
@@ -5,6 +5,7 @@ import (
 	dgctx "github.com/darwinOrg/go-common/context"
 	dglogger "github.com/darwinOrg/go-logger"
 	"os"
+	"time"
 )
 
 var DefaultClient *Client
@@ -16,13 +17,17 @@ func init() {
 }
 
 func Completion(ctx *dgctx.DgContext, request CompletionRequest) (CompletionResponse, error) {
+	start := time.Now().UnixMilli()
 	response, err := DefaultClient.CreateCompletion(context.Background(), request)
-	dglogger.Infof(ctx, "create completion, request: %+v, response: %+v, error: %v", request, response, err)
+	dglogger.Infof(ctx, "create completion, request: %+v, response: %+v, error: %v, cost: %d ms",
+		request, response, err, time.Now().UnixMilli()-start)
 	return response, err
 }
 
 func Chat(ctx *dgctx.DgContext, request ChatCompletionRequest) (ChatCompletionResponse, error) {
+	start := time.Now().UnixMilli()
 	response, err := DefaultClient.CreateChatCompletion(context.Background(), request)
-	dglogger.Infof(ctx, "create chat, request: %+v, response: %+v, error: %v", request, response, err)
+	dglogger.Infof(ctx, "create chat, request: %+v, response: %+v, error: %v, cost: %d ms",
+		request, response, err, time.Now().UnixMilli()-start)
 	return response, err
 }
