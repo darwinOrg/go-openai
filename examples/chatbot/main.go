250437d50c76aadd921a5f739fb7f6c56473b165
chenbiao <149027310@qq.com>
2023-10-17 17:02:30 +0800

    refactor

--- examples/chatbot/main.go
+++ examples/chatbot/main.go
@@ -17,23 +17,13 @@ func main() {
 		Messages: []openai.ChatCompletionMessage{
 			{
 				Role:    openai.ChatMessageRoleSystem,
-				Content: "我是一名猎头，我将给你上传一份访谈对话文本，访谈对话内容主要是猎头向企业雇主了解招聘需求，请你记住这份对话文本",
+				Content: "you are a helpful chatbot",
 			},
 		},
 	}
 	fmt.Println("Conversation")
 	fmt.Println("---------------------")
 	fmt.Print("> ")
-
-	resp, err := client.CreateChatCompletion(context.Background(), req)
-	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
-		return
-	}
-	fmt.Printf("%s\n\n", resp.Choices[0].Message.Content)
-	req.Messages = append(req.Messages, resp.Choices[0].Message)
-	fmt.Print("> ")
-
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		req.Messages = append(req.Messages, openai.ChatCompletionMessage{
